fbedde871516ac89e6f5eb4db0fda6d836f67b40
Jeff Horn <jhorn@reachtech.com>
2014-02-17 10:23:25 -0800

    Add Copyright Notice to all source files
    Signed-off-by: Jeff Horn <jhorn@reachtech.com>

--- mainview.h
+++ mainview.h
@@ -1,3 +1,14 @@
+/*************************************************************************
+**
+**    (C) Copyright 2013 Reach Technology Inc.
+**
+**    This code is protected by international copyright laws. This file may
+**    only be used in accordance with a license and cannot be used on
+**    hardware other than supplied by Reach Technology Inc. We appreciate
+**    your understanding and fairness.
+**
+*************************************************************************/
+
 #ifndef MAINVIEW_H
 #define MAINVIEW_H
 
