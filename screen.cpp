8c35b82c840768e5bc9cfe001e86c9b3ac18a86e
Chris Messineo <chris@chrismessineo.com>
2016-01-27 10:34:56 -0800

    Added a screensaver function to the screen.cpp and screen.h files.

--- screen.cpp
+++ screen.cpp
@@ -1,12 +1,96 @@
 #include "screen.h"
+#include <QDebug>
 
 Screen::Screen(QQuickView *parent) :
-    QObject(0), view(parent)
+    QObject(0), view(parent), m_screenSaverTimer(new QTimer(this))
 {
+    m_screenSaverEnabled = false;
+    QSettings settings(SYSTEM_SETTINGS_FILE,QSettings::NativeFormat);
+    settings.beginGroup(SYSTEM_SETTINGS_SECTION);
+    m_screenSaverTimeout = settings.value("screensaver_timeout",0).toInt();\
+    m_screenOriginalBrightness = settings.value("screen_original_brigtness",7).toInt();
+    m_screenDimBrightness = settings.value("screen_dim_brigtness",5).toInt();
+    settings.endGroup();
+    m_dim = false;
+
+    if (m_screenSaverTimeout > 0)
+    {
+        m_screenSaverEnabled = true;
+        //set the original brighness in case the device was shutdown in dim mode
+        setBrightness(m_screenOriginalBrightness);
+        connect(m_screenSaverTimer, SIGNAL(timeout()),this,SLOT(onScreenSaverTimerTimeout()));
+        view->installEventFilter(this);
+        m_screenSaverTimer->start(m_screenSaverTimeout * 60 * 1000);
+    }
 }
 
 bool Screen::save(const QString &path)
 {
     QImage image = view->grabWindow();
     return image.save(path, 0, 80);
+}
+
+void Screen::setOriginalBrightness()
+{
+    setBrightness(m_screenOriginalBrightness);
+    m_dim = false;
+    m_screenSaverTimer->stop();
+    m_screenSaverTimer->start(m_screenSaverTimeout * 60 * 1000);
+}
+
+bool Screen::isDim()
+{
+    return m_dim;
+}
+
+bool Screen::isScreenSaverEnabled()
+{
+    return m_screenSaverEnabled;
+}
+
+void Screen::onScreenSaverTimerTimeout()
+{
+    setBrightness(m_screenDimBrightness);
+    m_dim = true;
+}
+
+void Screen::setBrightness(int val)
+{
+    QFile brightness_file(BRIGHTNESS);
+    brightness_file.open(QIODevice::ReadWrite);
+    QTextStream out(&brightness_file);
+
+    out << QString::number(val).toLatin1() << endl;
+    brightness_file.close();
+
+}
+
+bool Screen::eventFilter(QObject *obj, QEvent *event)
+{
+    Q_UNUSED(obj);
+    switch (event->type()) {
+    case QEvent::KeyRelease:
+    case QEvent::KeyPress:
+    case QEvent::MouseButtonPress:
+    case QEvent::TouchBegin:
+    case QEvent::MouseMove:
+        if (m_dim)
+            return true;
+    case QEvent::MouseButtonRelease:
+        if (m_dim)
+        {
+            setOriginalBrightness();
+            return true;
+        }
+        else
+        {
+            m_screenSaverTimer->stop();
+            m_screenSaverTimer->start(m_screenSaverTimeout * 60 * 1000);
+        }
+        break;
+    default:
+        break;
+    }
+
+    return false;
 }
--- screen.h
+++ screen.h
@@ -4,6 +4,13 @@
 #include <QObject>
 #include <QImage>
 #include <QQuickView>
+#include <QTimer>
+#include <QSettings>
+#include "systemdefs.h"
+#include <QFile>
+#include <QTextStream>
+
+#define BRIGHTNESS "/sys/class/backlight/backlight.22/brightness"
 
 class Screen : public QObject
 {
@@ -15,10 +22,25 @@ signals:
 
 public slots:
      bool save(const QString &path);
+     void setOriginalBrightness();
+     bool isDim();
+     bool isScreenSaverEnabled();
+
+
+private slots:
+    void onScreenSaverTimerTimeout();
 
 private:
     QQuickView *view;
+    QTimer *m_screenSaverTimer;
+    int m_screenSaverTimeout;
+    int m_screenOriginalBrightness;
+    int m_screenDimBrightness;
+    bool m_dim;
+    bool m_screenSaverEnabled;
 
+    bool eventFilter(QObject *obj, QEvent *event);
+    void setBrightness(int val);
 };
 
 #endif // SCREEN_H
